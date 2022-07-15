be7417ab59a533b5c72fd493455a95d91b8db1e9
melih <melihkkaragoz@hotmail.com>
2022-07-15 23:24:52 +0300

    update

--- DART/DebugTools.dart
+++ DART/DebugTools.dart
@@ -8,15 +8,40 @@ class DebugTools {
     print("[DEBUG] $msg");
   }
 
-  void addFlag([String? label]) {
+  void seperate(String ch, [int count = 0]) {
+    print(count != 0 ? ch * count : ch * 20);
+  }
+
+  int addFlag([String? label]) {
     // dilerseniz label parametresi ile yeni flag'i isimlendirebilirsiniz.
     flagCount++;
     label != null
         ? print("program $label kismina kadar calisti.")
         : print("program $flagCount.flag kismina kadar calisti.");
+    return flagCount;
+  }
+
+  int getTheNumberOfFlags() {
+    return flagCount;
   }
 
   void resetFlag() {
     flagCount = 0;
   }
 }
+
+////////////////////////////////
+// Test alani
+
+// void main(List<String> args) {
+//   DebugTools dt = DebugTools(); // DebugTools nesnesi olusturuldu
+
+//   dt.addFlag(); // addFlag metodu denendi.
+//   dt.seperate('*'); // seperate metodu denendi.
+//   dt.addFlag();
+//   dt.seperate('~', 30);
+
+//   print("toplamda ${dt.getTheNumberOfFlags()} flag eklendi");
+// }
+
+////////////////////////////////
